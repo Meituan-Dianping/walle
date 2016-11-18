ed232b187f34246c301be77cd4d66c3027734fe6
陈潼 <chentong02@meituan.com>
2016-11-18 10:47:49 +0800

    基础接口确定不动，发版0.0.1

--- library/src/main/java/com/meituan/android/walle/ChannelReader.java
+++ library/src/main/java/com/meituan/android/walle/ChannelReader.java
@@ -2,54 +2,71 @@ package com.meituan.android.walle;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import java.io.File;
+import java.util.Map;
 
 public class ChannelReader {
+
+    @Nullable
+    public static String getChannel(@NonNull Context context) {
+        ChannelInfo channelInfo = getChannelInfo(context);
+        if (channelInfo == null) {
+            return null;
+        }
+        return channelInfo.getChannel();
+    }
+
+    @Nullable
+    public static String getChannelExtraInfoByKey(@NonNull Context context, @NonNull String key) {
+        ChannelInfo channelInfo = getChannelInfo(context);
+        if (channelInfo == null) {
+            return null;
+        }
+        return channelInfo.getExtraInfo().get(key);
+    }
+
     @Nullable
     public static ChannelInfo getChannelInfo(@NonNull Context context) {
+        String apkPath = getApkPath(context);
+        if (TextUtils.isEmpty(apkPath)) {
+            return null;
+        }
+        return PayloadReader.getChannelInfo(new File(apkPath));
+    }
+
+    @Nullable
+    public static String getChannelInfoByKey(@NonNull Context context, @NonNull String key) {
+        Map<String, String> channelMap = getChannelInfoMap(context);
+        if (channelMap == null) {
+            return null;
+        }
+        return channelMap.get(key);
+    }
+
+    @Nullable
+    public static Map<String, String> getChannelInfoMap(@NonNull Context context) {
+        String apkPath = getApkPath(context);
+        if (TextUtils.isEmpty(apkPath)) {
+            return null;
+        }
+        return PayloadReader.getChannelInfoMap(new File(apkPath));
+    }
+
+    @Nullable
+    private static String getApkPath(@NonNull Context context) {
         String apkPath = null;
         try {
-            ApplicationInfo applicationInfo = getApplicationInfo(context.getApplicationContext());
+            ApplicationInfo applicationInfo = context.getApplicationInfo();
             if (applicationInfo == null) {
                 return null;
             }
             apkPath = applicationInfo.sourceDir;
         } catch (Throwable e) {
         }
-
-        if (TextUtils.isEmpty(apkPath)) {
-            return null;
-        }
-        return PayloadReader.getChannel(new File(apkPath));
-    }
-
-    private static ApplicationInfo getApplicationInfo(Context context)
-            throws PackageManager.NameNotFoundException {
-        PackageManager pm;
-        String packageName;
-        try {
-            pm = context.getPackageManager();
-            packageName = context.getPackageName();
-        } catch (RuntimeException e) {
-            /* Ignore those exceptions so that we don't break tests relying on Context like
-             * a android.test.mock.MockContext or a android.content.ContextWrapper with a null
-             * base Context.
-             */
-//            Log.w(TAG, "Failure while trying to obtain ApplicationInfo from Context. " +
-//                    "Must be running in test mode. Skip patching.", e);
-            return null;
-        }
-        if (pm == null || packageName == null) {
-            // This is most likely a mock context, so just return without patching.
-            return null;
-        }
-        ApplicationInfo applicationInfo =
-                pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-        return applicationInfo;
+        return apkPath;
     }
 }
