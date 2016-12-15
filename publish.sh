cd8baf8d2621142c99f108d0cbcb80152a751481
陈潼 <chentong02@meituan.com>
2016-12-15 10:54:26 +0800

    更新一个发布脚本： 从gradle.properties中获取group  version 匹配后更新readme、打aar、输出可执行jar 、打tag

--- publish.sh
+++ publish.sh
@@ -4,14 +4,16 @@ cp walle-cli/build/libs/walle-cli-all.jar walle-cli/walle-cli-all.jar
 # 替换readme中的版本
 VERSION_NAME=`grep VERSION_NAME gradle.properties`
 VERSION_STRING=`echo $VERSION_NAME | cut -d '=' -f 2`
+GROUP=`grep GROUP gradle.properties`
+GROUP_STRING=`echo $GROUP | cut -d '=' -f 2`
 for file in $(find . -name 'README.md'); do
- sed -i.bak -e "s/\(com.meituan.android.walle\):\(.*\):.*/\1:\2:${VERSION_STRING}/g" ${file}
+ sed -i.bak -e "s/\(['\"]\)\(${GROUP_STRING}\):\(.*\):.*\(['\"]\)/\1\2:\3:${VERSION_STRING}\4/g" ${file}
  rm ${file}.bak
 done
 # 提交
 git add .
 git commit -m "update cli.jar & readme"
 # upload jar & aar
-./gradlew clean uploadArchives
+#./gradlew clean uploadArchives
 # git tag
 git tag v$VERSION_STRING
